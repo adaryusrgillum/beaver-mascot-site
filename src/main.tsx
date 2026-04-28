0d3b167d0ecaca2fafefebeb469220afcca7f9c0
adaryusrgillum <adaryusrgillum@users.noreply.github.com>
2026-04-28 13:38:25 -0400

    Fix BrowserRouter basename for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/beaver-mascot-site">
       <App />
     </BrowserRouter>
   </StrictMode>,
